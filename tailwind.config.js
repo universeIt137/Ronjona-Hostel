532b11cdb0a31cb93491e731c2f8157b08379454
ashikur <ashikur1602@gmail.com>
2025-01-04 17:27:20 +0600

    working with Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
     
   },
-  plugins: [require('daisyui'),],
+  plugins: [
+    require('daisyui'),
+    flowbite.plugin(),
+  ],
 }
