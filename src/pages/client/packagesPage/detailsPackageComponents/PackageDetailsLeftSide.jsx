c81463504f6b94f77fcf3f0a57a8123b23dc3bc6
Mahbul Hassan Sakib <mdsakibsardar222@gmail.com>
2025-01-09 14:23:17 +0600

    working on leftside banner

--- src/pages/client/packagesPage/detailsPackageComponents/PackageDetailsLeftSide.jsx
+++ src/pages/client/packagesPage/detailsPackageComponents/PackageDetailsLeftSide.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Bannar from './leftSideComponents/bannar';
 import PackageDetailsDescription from './leftSideComponents/PackageDetailsDescription';
 
 const PackageDetailsLeftSide = () => {
     return (
         <div className=''>
-            <Bannar/>
+            
             <PackageDetailsDescription/>
         </div>
     );
