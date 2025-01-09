b78bb2ee458a5a31cac815abe36f7bac99f5dfdc
Mahbul Hassan Sakib <mdsakibsardar222@gmail.com>
2025-01-09 14:32:50 +0600

    banner problem solved

--- src/pages/client/packagesPage/detailsPackageComponents/PackageDetailsLeftSide.jsx
+++ src/pages/client/packagesPage/detailsPackageComponents/PackageDetailsLeftSide.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PackageDetailsDescription from './leftSideComponents/PackageDetailsDescription';
+import Banner2 from './leftSideComponents/Banner2';
 
 const PackageDetailsLeftSide = () => {
     return (
         <div className=''>
-            
+            <Banner2/>
             <PackageDetailsDescription/>
         </div>
     );
