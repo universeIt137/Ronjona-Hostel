8214f1cbbbd696f85ccf119d42723c1dcc2b933e
ishanrana363 <ishanrana363@gmail.com>
2025-01-14 12:03:05 +0600

    work on home page packages sections

--- src/pages/client/homePage/homeComponents/Facility.jsx
+++ src/pages/client/homePage/homeComponents/Facility.jsx
@@ -1,53 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { FaDumbbell, FaParking, FaWifi } from 'react-icons/fa';
-import { GiWashingMachine } from 'react-icons/gi';
-import { HiLightBulb } from 'react-icons/hi';
-import { IoLogoGameControllerB } from 'react-icons/io';
-import { IoFastFoodOutline } from 'react-icons/io5';
-import { PiPersonSimpleSwimDuotone } from 'react-icons/pi';
+import useAxiosPublic from '../../../../hooks/useAxiosPublic';
 
 const Facility = () => {
+    const axiosPublic = useAxiosPublic();
+
+    const { data: featuresData = [], refetch, isError, isLoading } = useQuery({
+        queryKey: ['featuresData'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/getAllFeatures');
+            return res.data?.data;
+        }
+    });
+
+    if (isLoading) {
+        return (
+            <div className="flex flex-col justify-center items-center h-screen">
+                <h1>Loading Data....</h1>
+            </div>
+        );
+    }
+
     return (
-        <div className='mt-20 container px-4 md:px-0 md:mx-auto'>
-            <div className='  mb-10 '>
+        <div className="mt-16 w-11/12 mx-auto  px-4 md:px-0 md:mx-auto">
+            <div className="mb-10">
                 <p className="text-2xl md:text-4xl hover:underline font-bold text-main-color">Our Facilities</p>
                 <p>We offer modern (5 star) hotel facilities for your comfort.</p>
-                
             </div>
             <div>
-                <div className='grid grid-cols-2 md:grid-cols-4 gap-4 '>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <PiPersonSimpleSwimDuotone className='text-4xl m-4'></PiPersonSimpleSwimDuotone>
-                        <p>Swimming Pool</p>  
-                    </div>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <FaWifi className='text-4xl m-4'></FaWifi>
-                        <p>Wifi</p>  
-                    </div>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <IoFastFoodOutline className='text-4xl m-4'></IoFastFoodOutline>
-                        <p>Breakfast</p>  
-                    </div>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <FaDumbbell className='text-4xl m-4'></FaDumbbell>
-                        <p>Gym</p>  
-                    </div>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <IoLogoGameControllerB className='text-4xl m-4'></IoLogoGameControllerB>
-                        <p>Game Center</p>  
-                    </div>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <HiLightBulb className='text-4xl m-4'></HiLightBulb>
-                        <p>24/7 Light</p>  
-                    </div>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <GiWashingMachine className='text-4xl m-4'></GiWashingMachine>
-                        <p>Laundry</p>  
-                    </div>
-                    <div className=' justify-items-center bg-[#5a514f] text-white py-7 lg:py-16  '>
-                        <FaParking className='text-4xl m-4'></FaParking>
-                        <p>Parking Space</p>  
-                    </div>
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    {featuresData.map((item, i) => (
+                        <div key={i}>
+                            <div className="justify-items-center bg-[#5a514f] text-white py-7 lg:py-16 transform transition-transform duration-300 hover:scale-105">
+                                <div className="h-8">
+                                    <img className="w-12" src={item?.logo} alt={item?.title} />
+                                </div>
+                                <div className="lg:mt-8">
+                                    <p>{item?.title}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
