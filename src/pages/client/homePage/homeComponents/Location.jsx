7d82d648771cd273f4748d120055f26a473508aa
ishanrana363 <ishanrana363@gmail.com>
2025-01-14 11:14:12 +0600

    work on home page

--- src/pages/client/homePage/homeComponents/Location.jsx
+++ src/pages/client/homePage/homeComponents/Location.jsx
@@ -1,12 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import Marquee from 'react-fast-marquee';
+import useAxiosPublic from '../../../../hooks/useAxiosPublic';
 
 const Location = () => {
+    const axiosPublic = useAxiosPublic();
+
+    const { data: locationData = [], refetch, isError, isLoading } = useQuery({
+        queryKey: ['locationData'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/getAllLocations');
+            return res.data?.data;
+        }
+    });
+    console.log(locationData)
     return (
         <div className="mt-64 md:mt-16 container px-4 md:px-0 md:mx-auto">
             {/* Title */}
             <p className="hover:underline text-2xl md:text-4xl mb-6 font-bold text-main-color">Locations</p>
-            
+
             {/* Wrapper with overflow hidden */}
             <div className="overflow-hidden relative">
                 {/* Absolute element for larger screens */}
@@ -18,20 +30,20 @@ const Location = () => {
                     />
                 </div>
 
-                <Marquee speed={100} gradient={false}>
+                <Marquee speed={60} gradient={false}>
                     <div className="flex md:gap-4 gap-2 px-1  py-10">
                         {/* Cards */}
-                        {Array.from({ length: 8 }).map((_, index) => (
+                        {locationData.map((location, index) => (
                             <div
                                 key={index}
                                 className="flex-shrink-0 md:h-60 h-48 md:w-56 w-40 text-center p-4 bg-gray-200 rounded-2xl"
                             >
                                 <img
-                                    src="https://res.cloudinary.com/dntcuf8u3/image/upload/v1733452829/samples/imagecon-group.jpg"
-                                    alt="Location"
+                                    src={location.img}  // Assuming the location data includes an image URL
+                                    alt={location.location}       // Assuming the location data includes a name
                                     className="md:h-40 h-32 w-full object-cover rounded-lg"
                                 />
-                                <p className="p-4 text-sm md:text-base">Noida</p>
+                                <p className="p-4 text-sm md:text-base">{location.location}</p>
                             </div>
                         ))}
                     </div>
