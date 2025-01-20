4fd39dff08a3d8536d6fde096132a96b912c1296
ishanrana363 <ishanrana363@gmail.com>
2025-01-20 17:39:36 +0600

    packages details img added

---
+++ src/components/client/packages-details-images/PackagesDetailsImage.jsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+
+const PackagesDetailsImage = () => {
+    const axiosPublic = useAxiosPublic();
+
+    // Fetch data using useQuery
+    const { data: imagesData = [], isLoading } = useQuery({
+        queryKey: ['packageImages'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/getAllPackages');
+            return res.data.data; // Adjust based on API response structure
+        },
+    });
+
+    // Check if imagesData is available and has required data
+    const images = imagesData.length > 0 && imagesData[0]?.img
+        ? Object.keys(imagesData[0].img).map((key) => ({
+            original: imagesData[0].img[key],
+            thumbnail: imagesData[0].img[key],
+        }))
+        : [];
+
+    return (
+        <div>
+            {isLoading ? (
+                <p>Loading images...</p>
+            ) : (
+                <ImageGallery autoPlay={true} items={images} />
+            )}
+        </div>
+    );
+};
+
+export default PackagesDetailsImage;
