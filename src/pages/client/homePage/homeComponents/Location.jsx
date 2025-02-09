7dd92dae2fa538b6ea4782f58eac4709ce32efe0
ishanrana363 <ishanrana363@gmail.com>
2025-02-09 16:12:02 +0600

    location section dynamic and link tag added

--- src/pages/client/homePage/homeComponents/Location.jsx
+++ src/pages/client/homePage/homeComponents/Location.jsx
@@ -4,6 +4,7 @@ import Marquee from 'react-fast-marquee';
 import useAxiosPublic from '../../../../hooks/useAxiosPublic';
 import SkeletonLoader from '../../../../components/skeleton-loader/SkeletonLoader';
 import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 
 const Location = () => {
     const axiosPublic = useAxiosPublic();
@@ -45,12 +46,12 @@ const Location = () => {
                                 key={index}
                                 className="flex-shrink-0 md:h-60 h-48 md:w-56 w-40 text-center p-4 bg-gray-200 rounded-2xl"
                             >
-                                <img
+                                <Link to={"/our-branch"} ><img
                                     src={location.img}  // Assuming the location data includes an image URL
                                     alt={location.location}       // Assuming the location data includes a name
                                     className="md:h-40 h-32 w-full object-cover rounded-lg"
                                 />
-                                <p className="p-4 text-sm md:text-base">{location.location}</p>
+                                    <p className="p-4 text-sm md:text-base">{location.location}</p></Link>
                             </div>
                         ))}
                     </div>
