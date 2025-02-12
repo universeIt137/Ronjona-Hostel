9be943eaf8c238e4e78e9cf7cafd63ec985bf06d
ishanrana363 <ishanrana363@gmail.com>
2025-02-12 16:20:56 +0600

    work on price section

--- src/pages/admin/price-page/UploadPrice.jsx
+++ src/pages/admin/price-page/UploadPrice.jsx
@@ -4,6 +4,7 @@ import { createAlert } from "../../../helper/createAlert";
 import axios from "axios";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import toast, { Toaster } from "react-hot-toast";
+import PriceTable from "./PriceTable";
 
 const UploadPrice = () => {
     const axiosPublic = useAxiosPublic();
@@ -38,33 +39,41 @@ const UploadPrice = () => {
     };
 
     return (
-        <div className="max-w-md mx-auto p-6 bg-white shadow-lg rounded-lg mt-40 ">
-            <Helmet>
-                <title>Dashboard | Price Upload </title>
-            </Helmet>
-            <h2 className="text-xl font-semibold mb-4">Enter Price</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label className="block text-gray-700 font-medium mb-2">Price ($)</label>
-                    <input
-                        type="text"
-                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter price"
-                        name="price"
-                        required
-                    />
+        <>
+            <div className="max-w-md mx-auto p-6 bg-white shadow-lg rounded-lg mt-40 ">
+                <Helmet>
+                    <title>Dashboard | Price Upload </title>
+                </Helmet>
+                <h2 className="text-xl font-semibold mb-4">Enter Price</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 font-medium mb-2">Price ($)</label>
+                        <input
+                            type="text"
+                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            placeholder="Enter price"
+                            name="price"
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+                    >
+                        {
+                            loading ? "Loading..." : "Submit"
+                        }
+                    </button>
+                </form>
+                <div>
+
                 </div>
-                <button
-                    type="submit"
-                    className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
-                >
-                    {
-                        loading ? "Loading..." : "Submit"
-                    }
-                </button>
-            </form>
-            <Toaster position="top-center"></Toaster>
-        </div>
+                <Toaster position="top-center"></Toaster>
+            </div>
+            <div className="p-10" >
+                <PriceTable></PriceTable>
+            </div>
+        </>
     );
 };
 
